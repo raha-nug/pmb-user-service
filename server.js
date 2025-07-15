00c9ef61333ef6a424cd2ee594e5528d07a1128e
raha-nug <rival10221029@student.sttcipasung.ac.id>
2025-07-15 12:35:31 +0700

    create config for vercel deploy

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 import userRoutes from "./src/interfaces/routes/userRoute.js";
 import userAuth from "./src/interfaces/routes/authRoute.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
