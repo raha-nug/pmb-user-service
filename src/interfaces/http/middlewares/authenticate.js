d10af63eac8e8b37bdecc22445ef4b4896b13464
raha-nug <rival10221029@student.sttcipasung.ac.id>
2025-07-20 12:20:56 +0700

    change status code

--- src/interfaces/http/middlewares/authenticate.js
+++ src/interfaces/http/middlewares/authenticate.js
@@ -5,7 +5,7 @@ export const authenticateToken = (req, res, next) => {
   const token = authHeader && authHeader.split(" ")[1]; // Bearer TOKEN
 
   if (token == null) {
-    return res.status(403).json({
+    return res.status(401).json({
       message: "Harap masukan token",
     }); // Unauthorized
   }
