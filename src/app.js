74c6300a7016085d6606bd418cc83a15134c6905
raha-nug <rival10221029@student.sttcipasung.ac.id>
2025-07-24 18:08:20 +0700

    fix error

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ import userAuth from "./interfaces/routes/authRoute.js";
 const app = express();
 
 app.use(cors());
-app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
